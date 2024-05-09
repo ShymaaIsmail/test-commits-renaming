223093a77f9103fa57a579a74c496adde1e33dec
shymaaismail <shymaa.m.ismail@gmail.com>
2024-05-09 17:37:50 +0300

    do

--- kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/settings.py
+++ kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-50&!ch468!jm0xg6@krt!hp%w9f_3cq%uxp2=8m7jy@&&-ajm8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', '*']
 
 
 # Application definition
