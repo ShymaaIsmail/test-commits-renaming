cdb1bb09074d7c2ba4448368e8ca4dbbc4b275e2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-05-09 17:57:52 +0300

    python3  manage.py collectstatic

--- kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/settings.py
+++ kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'django-static/'
+STATIC_ROOT = '/data/web_static/current'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
